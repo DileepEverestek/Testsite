591ebc183f1fca00e6ff1beec2e012dd2bf1ae1b
DileepEverestek <100899517+DileepEverestek@users.noreply.github.com>
2022-03-11 18:55:41 +0530

    env file test

--- src/database/database.py
+++ src/database/database.py
@@ -2,7 +2,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Dileep77@localhost/testfastapi"
+from src.core.config import settings
+
+
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+
+#SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:Dileep77@localhost/testfastapi"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
